969630671b72b210f2c5c7bb77d53ffa745df503
Francesco-Manzoni <francesco.manzoni1996@gmail.com>
2021-03-08 19:02:13 +0100

    Aggiunto modello utente e iniziate api del profilo

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+// @route GET api/profile/me
+// @desc Get profilo utente attuale
+// @access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }) //Ottengo profilo utente dall'userid
+      .populate('user', ['name', 'avatar']); //popolo anche il campo user con nome e avatar
 
-// @route GET api/profile
-// @desc test route
-// @access Public
-router.get('/', (req, res) => res.send('Profile route'));
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'Profilo non trovato per questo utente' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server avviato sulla porta ${PORT}`));
