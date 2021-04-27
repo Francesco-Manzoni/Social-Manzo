c26e708f7be0029101878ea327122ff922684db2
Francesco-Manzoni <francesco.manzoni1996@gmail.com>
2021-04-27 13:15:50 +0200

    alcuni fix

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <h1 className='x-large text-primpary'>
+        <i className='fas fa-exclamation-triangle'> Pagina non trovata</i>
+      </h1>
+    </Fragment>
+  );
+};
+
+export default NotFound;
