a09a00c2eb4c822769a46bb644614b126a8e8265
Francesco Manzoni <francesco.manzoni@ddmsolutions.it>
2021-06-15 08:55:51 +0200

    preaparzione deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./Config/db');
+const path = require('path');
 
 const app = express(); // serve per far andare il server
 
@@ -10,13 +11,23 @@ connectDB();
 app.use(express.json({ xtended: false }));
 
 //Creo una risposta ad una get sulla porta 5000
-app.get('/', (req, res) => res.send('API partita'));
+//app.get('/', (req, res) => res.send('API partita'));
 
 // Definisco i percorsi delle API
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//Per heroku
+if (process.env.NODE_ENV === 'production') {
+  //imposto cartella statica
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
